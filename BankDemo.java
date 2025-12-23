6693124399def5fd4565d398f0943e018e4e962b
Ranjit <ran31784@gmail.com>
2025-12-23 21:44:53 +0530

    allowing user to enter thier own details in bank, while other operations are same

--- BankDemo.java
+++ BankDemo.java
@@ -1,74 +1,106 @@
-
 import java.util.Scanner;
+
 class BankAccount{
-    int balance;
-    void Deposit(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the deposit amount");
-        int amount = sc.nextInt();
-        balance += amount;
-        System.out.println("Deposited successfully, now the current balance is :"+ balance);
+    private int balance;
+    private String cust_name;
+    private int Acc_no;
+
+    BankAccount(int balance, String cust_name, int Acc_no){
+        this.balance = balance;
+        this.cust_name = cust_name;
+        this.Acc_no = Acc_no;
     }
-    void Withdraw(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the withdrawal amount");
-        int amount = sc.nextInt();
-        if(balance >= amount){
+    void deposit(int amount){
+        if(amount > 0){
+        balance += amount;
+        System.out.println("Deposited successfully, now the current balance is :" +balance);
+    }
+        else{
+            System.out.println("Incorrect amount");
+        }
+    }
+    void withdraw(int amount){
+        if(amount > 0 && balance >= amount){
             balance -= amount;
             System.out.println("Withdrawal succesffuly, current balance is:" +balance);
         }
         else{
-            System.out.println("Insuffiicient balance");
+            System.out.println("Insuffiicient balance or invalid input");
         }
     }
-    void displaybalance(){
+    void display(){
         System.out.println("Balance is :" +balance);
+    }
+    int getBalance(){
+        return balance;
     }
 }
 class ATM{
-    BankAccount bankaccount1 = new BankAccount();
-    void depositcash(){
-        bankaccount1.Deposit();
+    BankAccount account;
+    ATM(BankAccount account){
+        this.account = account;
     }
-    void Withdraw(){
-        bankaccount1.Withdraw();
+    void deposit(int amt){
+        account.deposit(amt);
     }
-    void Display(){
-        bankaccount1.displaybalance();
+    void withdraw(int amt){
+        account.withdraw(amt);
+    }
+    void display(){
+        account.display();
     }
 }
 public class BankDemo{
     public static void main(String[] args){
-        ATM card1 = new ATM();
+        
+        Scanner sc = new Scanner(System.in);
+
+         System.out.println("Enter cust_name");
+         String cust_name = sc.nextLine();  
+         System.out.println("Enter Acc_no");
+         int Acc_no = sc.nextInt();  
+         System.out.println("Enter balance");
+         int balance = sc.nextInt();  
+         sc.nextLine();
+
+        BankAccount acc = new BankAccount(balance, cust_name, Acc_no );
+
+        System.out.println("Welcome "+ cust_name + " your account number is " +  Acc_no +  " and your balance is "+ balance +".");
+        ATM atm = new ATM(acc);
+
         System.out.println("1. Deposit");
         System.out.println("2. Withdraw");
         System.out.println("3. Display Balance");
         System.out.println("4. EXIT");
 
-   int a;
+   int choice;
     do { 
-         Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
          System.out.println("Enter desire choice");
-         a = sc.nextInt();  
+         choice = sc.nextInt();  
         
-        switch (a) {
+        switch (choice) {
             case 1:
-                card1.depositcash();   
+                System.out.println("Enter amount");
+                atm.deposit(sc.nextInt());   
                 break;
             case 2: 
-                card1.Withdraw();
+                System.out.println("Enter amount");
+                atm.withdraw(sc.nextInt());
                  break;
             case 3:
-                 card1.Display();
+                 atm.display();
                  break;
 
             case 4:
-                System.out.println("Exiting"); 
+                System.out.println("Dear " +cust_name+" your latest balance is "+ acc.getBalance() );
+                System.out.println("Exiting");
                 break;   
             default:
                 System.out.println("Invalid input");
         }
-    } while(a!=4);
+    } while(choice!=4);
+
 }
 
 }
