f355cc0b6242815f2247d6af1db3376249373245
Ranjit <ran31784@gmail.com>
2025-12-26 21:48:31 +0530

    used method in array list to make code reusable and clean

--- ArrayListwithMethods.java
+++ ArrayListwithMethods.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+class ArrayOperation{
+     ArrayList<Integer> number;
+     Scanner sc;
+     ArrayOperation(ArrayList<Integer> number , Scanner sc){
+        this.number = number;
+        this.sc = sc;
+     }
 
-
-public class Array{
-    public static void main(String[] args)
-    {
-         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> number = new ArrayList<>();
-        while(true){
-
-
-       
-        int index;
-        int element;
-        int index1;
-        int index2;
-        int sum = 0;
-
+        void Menu(){
             System.out.println("1. Add number ");
             System.out.println("2.Updating Elements");
             System.out.println("3.Add number of array");
@@ -26,13 +18,9 @@ public class Array{
             System.out.println("6.View elements");
             System.out.println("7.Exit");
 
-
-            int choice;
-            choice = sc.nextInt();
-            switch(choice){
-                case 1 :
-
-                    System.out.println("Do you want to add number in existing array? Enter Y for yes, and N to delete existing array element.");
+        }
+        void AddNumbers(){
+            System.out.println("Do you want to add number in existing array? Enter Y for yes, and N to delete existing array element.");
                     char op = sc.next().charAt(0);
                     
                     if(op == 'y' || op == 'Y'){
@@ -48,36 +36,34 @@ public class Array{
                     sc.next();
                 }
             }
-                    else if(op == 'N' || op == 'n' ){
+            else if(op == 'N' || op == 'n' ){
                     number.clear();
                     System.out.println("Array elements fully deleted, type 1 for adding new elements");
                 }
                     else{
                         System.out.println("Invalid input, Enter Y or N only ");
                     }
-                   
-        
-                break;
-
-                case 2 :
-                    System.out.println("At what index you want to update");
-                    index = sc.nextInt();
+        }
+        void UpdateElement(){
+             System.out.println("At what index you want to update");
+                    int index = sc.nextInt();
                     System.out.println("Enter the value");
-                    element = sc.nextInt();
+                    int element = sc.nextInt();
                     number.set(index, element);
                     System.out.println("Value" +element+ "at index " +index+ "is updated");
-                break;
 
-                case 3:
-                    System.out.println("Enter 0 for two index sum, 0 for whole array sum");
+        }
+        void SumNumber(){
+            int sum = 0;
+            System.out.println("Enter 1 for two index sum, 0 for whole array sum");
                     int abc = sc.nextInt();
                 if(abc == 1){
                     System.out.println("Enter first index");
-                    index1 = sc.nextInt();
+                    int index1 = sc.nextInt();
                     System.out.println("Enter second index");
-                    index2 = sc.nextInt();
+                    int index2 = sc.nextInt();
 
-                    sum = number.get(index2) + number.get(index1);
+                     sum = number.get(index2) + number.get(index1);
 
                     System.out.println("sum is" +sum);
                 }
@@ -85,7 +71,7 @@ public class Array{
                     int count = number.size();
                     for(int i = 0; i < count; i++){
                         
-                        sum = sum + number.get(i);
+                       sum = sum + number.get(i);
                         
                     }
                     System.out.println("Sum of whole array is " +sum);
@@ -94,14 +80,14 @@ public class Array{
                         System.out.println("inalid input, enter 0 or 1 only");
                     }
                     
-                break;
 
-                case 4:
-                    System.out.println("Enter index for you want to swap");
-                    index1 = sc.nextInt();
+        }
+        void SwapNumber(){
+             System.out.println("Enter index for you want to swap");
+                    int index1 = sc.nextInt();
                     int a = number.get(index1);
                     System.out.println("Enter the index you want to swap");
-                    index2 = sc.nextInt();
+                    int index2 = sc.nextInt();
                     int b = number.get(index2);
                     int temp = a;
                      a = b;
@@ -109,32 +95,62 @@ public class Array{
                      number.set(index1, a);
                      number.set(index2, b);
                      System.out.println("Swapped successfully" +number);
-                break;
-
-                case 5:
-                    System.out.println("Enter index to remove");
-                    index = sc.nextInt();
+        }
+        void Removenumber(){
+             System.out.println("Enter index to remove");
+                    int index = sc.nextInt();
                     number.remove(index);
                     System.out.println("Number removed succesffuly at index" + index);
-                break;
-
-                case 6:
-                    System.out.println(number);
-                break;
-
-                case 7:
-                    System.out.println("Exiting");
-                    System.exit(0);
-                break;
-
-                default:
-                    System.out.println("Invalid input");
-
-            
-            }
-
- 
-           }
-        
         }
+        void ViewElements(){
+            System.out.println(number);
+        }
+        void Exit(){
+            System.out.println("Exiting");
+                    System.exit(0);
+        }
+
+
+}
+
+public class ArrayListwithMethods{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> number1  = new ArrayList<>();
+        ArrayOperation oc = new ArrayOperation(number1, sc);
+        
+
+        while(true){
+            oc.Menu();
+            int op = sc.nextInt();
+            switch(op){
+
+            case 1: 
+                oc.AddNumbers();
+            break;
+            case 2:
+                oc.UpdateElement();
+            break;
+            case 3:
+                oc.SumNumber();
+            break;
+            case 4:
+                oc.SwapNumber();
+            break;
+            case 5:
+                 oc.Removenumber();
+            break;
+            case 6:
+                oc.ViewElements();
+            break;
+            case 7:
+                oc.Exit();
+            break;
+
+            default:
+                System.out.println("Invalid input");
+        }
+    }
+
+    }
 }
